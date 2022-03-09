709b94bb95823271ec1801fb8a987ce0f470ccf5
Trannguyentuan <50980766+Trannguyentuan@users.noreply.github.com>
2022-03-10 01:16:38 +0700

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        header('location: views/home.php');
+        ?>
+    </body>
+</html>
